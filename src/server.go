538cc297f80085ee78257aef9e099993367fee1f
olaf michaelis <o.mic@web.de>
2021-11-16 14:46:27 +0100

    Add query params parser

--- src/server.go
+++ src/server.go
@@ -8,23 +8,34 @@ import (
 )
 
 type tResponse struct {
-	Method string
-	Proto  string
-	Host   string
-	URL    string
+	Method      string
+	Proto       string
+	Host        string
+	URL         string
+	QueryParams map[string][]string
 }
 
 type handler struct{}
 
 func (h *handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	data := tResponse{
-		Method: req.Method,
-		Proto:  req.Proto,
-		Host:   req.Host,
-		URL:    fmt.Sprintf("%s", req.URL),
+		Method:      req.Method,
+		Proto:       req.Proto,
+		Host:        req.Host,
+		URL:         fmt.Sprintf("%s", req.URL),
+		QueryParams: queryParams(resp, req),
 	}
+
 	log.Printf("[INFO] %s %s %s%s", data.Method, data.Proto, data.Host, data.URL)
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusCreated)
 	json.NewEncoder(resp).Encode(data)
+}
+
+func queryParams(w http.ResponseWriter, r *http.Request) (qp map[string][]string) {
+	qp = make(map[string][]string)
+	for k, v := range r.URL.Query() {
+		qp[k] = v
+	}
+	return
 }
